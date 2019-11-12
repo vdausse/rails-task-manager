726bc7d70989efa5e2a20ecf4fdb2de19fdc4b9a
Valentin DAUSSE <valentindausse@gmail.com>
2019-11-12 17:15:19 +0100

    exo2

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,18 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+end
